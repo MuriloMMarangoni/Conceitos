f00c60417ed2542a035799e7174dee24031b442d
MuriloMMarangoni <3msxxvi@gmail.com>
2024-10-04 20:38:32 -0300

    Signed-off-by: MuriloMMarangoni <3msxxvi@gmail.com>

--- Java.java
+++ Java.java
@@ -9,31 +9,37 @@ public class Java{ // This class need to have the name of the file
         String ss = ""; // Inicializa uma string
         double d = 1.1; // Double
         float f = 1.123f; // Float
-        int i = 1; // Int
+        int in = 1; // Int
         char c = 'a'; // char
         boolean b = true; // bool
-        i = i + 1; // incremento
-        i++; // incremento
-        i--; // decremento
+        in = in + 1; // incremento
+        in++; // incremento
+        in--; // decremento
         Scanner teclado = new Scanner(System.in); // objeto que pega o input
-        i = teclado.nextInt(); // pega o input de um int
+        in = teclado.nextInt(); // pega o input de um int
         f = teclado.nextFloat(); // pega o input de um float
         s = teclado.next(); // pega o input de uma String
-        System.out.printf("%f%f%d",d,f,i); // print formatado (double,float,int)
+        teclado.close(); // ? faz o scanner parar de ler input
+        System.out.printf("%f%f%d",d,f,in); // print formatado (double,float,int)
         System.out.printf("%.2f", f); // 2 casas decimais
         System.out.printf("%s",s); // print formatado(string)
-        if (i == 0){ // if, else if, else 
+        if (in == 0){ // if, else if, else 
             System.out.println("É zero");
         }
-        else if (i < 0){
+        else if (in < 0){
             System.out.println("É negativo");
         }
         else {
             System.out.println("É positivo");
         }
-        while(i != 0){ // while
-            System.out.printf("%d\n",i);
-            i--;
+        while(in != 0){ // while
+            System.out.printf("%d\n",in);
+            in--;
         }
+        for (int i = 0;i<10;i++) { // for loop
+            System.out.println(i);
+        }
+        int arr[] = {1,2,3}; // array
+        arr[0] = 0; // acessa
     }
 }
